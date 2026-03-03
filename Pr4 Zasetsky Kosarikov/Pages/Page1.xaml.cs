503cf61b3b2c6429292498772c2eb76abb174a30
Reflex414 <9629970659rus@mail.ru>
2026-03-03 21:07:59 +0300

    Сделал mainwindow.xaml и сразу создал 3 пустых страницы и добавил два скрина функций

---
+++ Pr4 Zasetsky Kosarikov/Pages/Page1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Pr4_Zasetsky_Kosarikov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pr4 Zasetsky Kosarikov/Pages/Page2.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Pr4_Zasetsky_Kosarikov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
